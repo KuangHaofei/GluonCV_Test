a305dde06796031dbdda8edf7aa9454c794e7191
Kuang <haofeik@f8ffc22b1e09.ant.amazon.com>
2020-08-19 14:15:13 +0800

    debug code

---
+++ monodepthv2/weights/mono_debug/depth_weights_test.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+import mxnet as mx
+
+
+if __name__ == '__main__':
+    epoch0_depth_path = '../weights/mono_debug/epoch0_depth.params'
+    epoch10_depth_path = '../weights/mono_debug/epoch10_depth.params'
+
+    epoch0_depth = mx.nd.load(epoch0_depth_path)
+    epoch10_depth = mx.nd.load(epoch10_depth_path)
+
+    diff = 0
+
+    for key in epoch0_depth.keys():
+        if key == 'encoder.encoder.fc.weight' or key == 'encoder.encoder.fc.bias':
+            continue
+
+        epoch0_depth_layer = epoch0_depth[key].asnumpy()
+        epoch10_depth_layer = epoch10_depth[key].asnumpy()
+
+        layer_diff = np.linalg.norm(epoch0_depth_layer - epoch10_depth_layer)
+        diff += layer_diff
+
+    print("depth weights diff: ", diff)
+
+    epoch0_pose_path = '../weights/mono_debug/epoch0_pose.params'
+    epoch10_pose_path = '../weights/mono_debug/epoch10_pose.params'
+
+    epoch0_pose = mx.nd.load(epoch0_pose_path)
+    epoch10_pose = mx.nd.load(epoch10_pose_path)
+
+    diff = 0
+    for key in epoch0_pose.keys():
+        if key == 'encoder.encoder.fc.weight' or key == 'encoder.encoder.fc.bias':
+            continue
+
+        epoch0_pose_layer = epoch0_pose[key].asnumpy()
+        epoch10_pose_layer = epoch10_pose[key].asnumpy()
+
+        layer_diff = np.linalg.norm(epoch0_pose_layer - epoch10_pose_layer)
+        diff += layer_diff
+
+    print("pose weights diff: ", diff)
